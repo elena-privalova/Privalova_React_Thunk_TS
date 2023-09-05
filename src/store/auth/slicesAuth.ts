c74700da915c573f546a2b8f7681c4a96fc0723f
Elena Privalova <e.privalova@tg.dunice.net>
2023-09-05 15:48:08 +0300

    Deleted excess else

--- src/store/auth/slicesAuth.ts
+++ src/store/auth/slicesAuth.ts
@@ -40,10 +40,10 @@ export const authSlice = createSlice({
       })
       .addCase(signUpUser.fulfilled, (state, action) => {
         state.isLoading = false;
-        if (typeof action.payload === 'string') state.error = action.payload;
+        if (action.payload === 'string') state.error = action.payload;
         else {
           state.user = action.payload as AuthUser;
-          state.error = ''
+          state.error = '';
         };
       })
       .addCase(signUpUser.rejected, (state, action) => {
--- src/utils/getFormattedAvatarPath.ts
+++ src/utils/getFormattedAvatarPath.ts
@@ -2,8 +2,6 @@ export const getFormattedAvatarPath = (avatarPath: string | null): string => {
   if (avatarPath != null) {
     return `${import.meta.env.VITE_APP_API_URL}${avatarPath}`;
   }
-  else {
-    return '';
-  }
+  return '';
 };
 
